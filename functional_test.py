2e29c487c75bd77948d14732b8f5cc19f8c29ad1
mwytock0812 <michael.wytock@gmail.com>
2015-12-12 09:48:57 -0500

    Refactor FTs to use helper for finding table rows

--- functional_test.py
+++ functional_test.py
@@ -8,8 +8,17 @@ class NewVisitorTest(unittest.TestCase):
         self.browser = webdriver.Firefox()
         self.browser.implicitly_wait(3)
 
+
     def tearDown(self):
         self.browser.quit()
+
+
+    def check_for_row_in_list_table(self, row_text):
+        """Helper method for locating right table row"""
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
+
 
     def test_can_start_a_list_and_retrieve_it_later(self):
         # Edith has heard about a cool new online to-do app.
