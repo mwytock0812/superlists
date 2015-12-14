ede11aa1338eb459d05d7430dfae3325e5d80cc7
mwytock0812 <michael.wytock@gmail.com>
2015-12-14 17:44:05 -0500

    Add helper fxn to check text in table entries

--- functional_test.py
+++ functional_test.py
@@ -29,7 +29,7 @@ class NewVisitorTest(unittest.TestCase):
         self.assertIn('To-Do', self.browser.title)
         header_text = self.browser.find_element_by_tag_name('h1').text
         self.assertIn('To-Do', header_text)
-        
+
         # She is invited to enter a to-do item straight away
         inputbox = self.browser.find_element_by_id('id_new_item')
         self.assertEqual(
@@ -44,17 +44,14 @@ class NewVisitorTest(unittest.TestCase):
         # When she hits enter, the page updates, and now the page lists
         # "1: Buy peacock feathers" as an item in a to-do list
         inputbox.send_keys(Keys.ENTER)
-
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
 
         # There is still a text box inviting her to add another item. She
         # enters "Use peacock feathers to make a fly" (Edith is methodical)
         inputbox = self.browser.find_element_by_id('id_new_item')
         inputbox.send_keys('Use peacock feathers to make a fly')
         inputbox.send_keys(Keys.ENTER)
-        
+
         # The page updates again, and now shows both items on her list
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
@@ -63,7 +60,7 @@ class NewVisitorTest(unittest.TestCase):
             '2: Use peacock feathers to make a fly',
             [row.text for row in rows]
         )
-        
+
         # Edith wonders whether the site will remember her lsit. Then she
         # sees that the site has generate a unique URL for her -- there
         # is some explanatory txt to that effect.
